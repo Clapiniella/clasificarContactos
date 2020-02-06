9ce2fdce8943fe27ca4cfe7ee54d20e6465e10e0
nachorod11 <nacho.rod11@hotmail.com>
2020-02-06 11:11:40 +0100

    creamos main.r

---
+++ miscellaneous/main.R
@@ -0,0 +1,14 @@
+Directorio <- "C:/Users/nacho/Desktop/The_Bridge/temario/DATA_NOV2019-master/R/clasificarContactos/"
+
+setwd(Directorio)
+carpetaScripts <- paste0(Directorio, "R/")
+
+#List files nos devuelve los archivos dentro del directorio.
+
+lapply(paste0("R/", list.files(path = "R/", recursive = TRUE)), source)
+
+#debug(clasificarContactosApp)
+
+clasificarContactosApp(Directorio)
+
+#undebug(clasificarContactosApp)
