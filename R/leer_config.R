6961b654a24af1e6ef3be6777f24fdf0d5fda96c
nachorod11 <nacho.rod11@hotmail.com>
2020-02-06 11:19:52 +0100

    leer config actualizado

--- R/leer_config.R
+++ R/leer_config.R
@@ -7,11 +7,13 @@
 #' @return
 #' 
 #' @import XML
+#' @import logging
 #'
 #' @examples
 leerConfig <- function(path){
   
   library(XML)
+  
   configPath <- paste0(path, "config/config.xml")
   
   tryCatch(expr = {
@@ -30,8 +32,49 @@ leerConfig <- function(path){
   })
   
   loginfo("Config leido", logger = 'log')
+  browser()
+  validateConfigNodes(config)
   
   config$columnas$predictorasNumericas <- trimws(strsplit(config$columnas$predictorasNumericas, ",")[[1]])
-  loginfo("Llegas aqui=?", logger = 'log')
   
-}
+  return(config)
+  
+  }
+  
+#' @Title validateConfigNodes
+#'
+#' @param config 
+#'
+#' @import logging
+#'
+validateConfigNodes <- function(config){
+    
+    nodoPrincipal <- identical(names(config), c("input", "columnas"))
+    
+   nodoInput <- identical(names(config$input), c("name", "sep"))
+   
+   nodoColumnas <- identical(names(config$columnas), c("predictorasNumericas","fuenteOriginal",
+                                                       "dominio_mail", "fechas", "target", "llamada"))
+   
+   
+   nodoFechas <- identical(names(config$columnas$fechas), c("creacion", "ultima_mod", "envio_ultimo",
+                                                            "apertura_primero", "envio_primero",
+                                                            "visita_primero", "visita_ultimo",
+                                                            "tiempos", "visita_primero", "visita_ultimo" ))
+   
+   
+   nodos <- c("nodoPrincipal" = nodoPrincipal, "nodoInput" = nodoInput,
+              "nodoColumnas" = nodoColumnas, "nodoFechas" = nodoFechas)
+   
+   check <- all(nodos)
+   
+   if(!check){
+     
+     nodosMalos <- names(nodos)[!nodos]
+     
+     logerror(paste0("Los nodos, ",paste(nodosMalos, collapse = ",") , " estan mal estructurados!"),
+              logger = 'log')
+     
+     stop()
+   }
+  }
